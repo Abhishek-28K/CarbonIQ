5188bdf4f70b99f907aad8a318505e3139ac33a0
Abhishek <abhishekkumbhar167@apsit.edu.in>
2025-02-09 17:05:40 +0530

    basic changes

--- CarbonIQ/settings.py
+++ CarbonIQ/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authapp',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'CarbonIQ.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates',BASE_DIR / 'CarbonIQ/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +118,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS =[BASE_DIR /"static",]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587  
+EMAIL_HOST_USER = 'abhishekkumbhar005@gmail.com'
+EMAIL_HOST_PASSWORD = 'xkyw xebh fyae fbjh'
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
+
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# The URL path for accessing the media files
+MEDIA_URL = '/media/'
--- CarbonIQ/urls.py
+++ CarbonIQ/urls.py
@@ -16,7 +16,39 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from authapp import views as aviews
+from django.conf import settings
+from django.conf.urls.static import static
+from mainapp import views as mviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+authappurl = [
+    path('', aviews.landingpage, name="landingpage"),
+    path('home/', aviews.landingpage, name='home'),
+    path('login/', aviews.loginpage, name='login'), 
+    path('signup/', aviews.signuppage, name="signup"),
+    path('view_profile/<int:user_id>/', aviews.view_profile, name='view_profile'),
+    path('edit_profile/<int:user_id>/', aviews.edit_profile, name='edit_profile'),
+    path('logout/', aviews.user_logout, name='logout'),
+    path('forgotpassword/', aviews.ForgotPassword, name='forgotpassword'),
+    path('ChangePassword/<uuid:token>/', aviews.ChangePassword, name='changepassword'),
+    path('avatar_selection/', aviews.avatar_selection, name='avatar_selection'),
+    path('update-avatar/', aviews.update_avatar, name='update_avatar'),
+    path('add_friend/<int:user_id>/', aviews.add_friend, name='add_friend'),
+    path('friends_list/', aviews.friends_list, name='friends_list'),
+    path('accept_request/<int:request_id>/', aviews.accept_request, name='accept_request'),
+    path('decline_request/<int:request_id>/', aviews.decline_request, name='decline_request'),
+    path('search/', aviews.search_users, name='search_users'),
+]
+
+mainappurl = [
+    path('homepage/', mviews.homepage, name='homepage'),
+    path('analyze/', mviews.analyze, name='analyze'),
+]
+urlpatterns = urlpatterns + authappurl + mainappurl
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
